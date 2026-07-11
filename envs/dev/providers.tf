39313b08f27572b44f39edd0c73c6f4ebf9de39c
22taran <22tarandeep@gmail.com>
2026-07-11 12:32:10 -0400

    feat: add slack-lambda module (ack+worker, function url, dlq)

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.27"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
   }
   # backend "s3" {}  # configure per-team; left local for first apply
 }
